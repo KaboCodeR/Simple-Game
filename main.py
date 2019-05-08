840c860079b83f38961c7debb2f5b1a0687a51f1
KaboCodeR <50385956+KaboCodeR@users.noreply.github.com>
2019-05-07 17:25:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import turtle
+
+wn = turtle.Screen()
+turtle = turtle.Turtle()
+
+wn.bgcolor("black")
+turtle.shape("square")
+turtle.color("white")
+
+def gotomiddle():
+  turtle.goto(0, 0)
+  
+  
+while True:
+  turtle.goto(-150, 150)
+  gotomiddle()
